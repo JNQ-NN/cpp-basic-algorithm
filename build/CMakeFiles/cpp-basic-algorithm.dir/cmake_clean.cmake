3c36fe956d4437e3e6bbdd0e3407d5af88aba8d5
JNQ_orz <13791344+jnq-orz@user.noreply.gitee.com>
2023-12-03 22:59:43 +0800

    init(global):环境初始化

---
+++ build/CMakeFiles/cpp-basic-algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp-basic-algorithm.dir/main.cpp.o"
+  "CMakeFiles/cpp-basic-algorithm.dir/main.cpp.o.d"
+  "cpp-basic-algorithm"
+  "cpp-basic-algorithm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp-basic-algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
